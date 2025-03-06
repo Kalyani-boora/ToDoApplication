3111565750ea552f1854843d87fd60cbbce0f012
Kalyani <boorakalyani20@gmail.com>
2025-03-06 15:34:27 +0530

    Defines ToDo task model

---
+++ src/main/java/com/example/ToDoApplication/Entity/ToDoEntity.java
@@ -0,0 +1,45 @@
+package com.example.ToDoApplication.Entity;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
+public class ToDoEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String title;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date date;
+	private String status;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+} 
